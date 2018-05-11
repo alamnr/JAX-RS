9156d99ce591691d38a3b7529f4c40a6092031bd
alamnr <alamnr@gmail.com>
2018-05-11 15:08:25 +0600

    Commit for REST Web Service-17 Acessing Path Param

--- src/main/java/org/koushik/javabrains/JAX_RS/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/JAX_RS/resources/MessageResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,15 +13,22 @@ import org.koushik.javabrains.JAX_RS.service.MessageService;
 
 @Path("/messages")
 public class MessageResource {
-	
-	MessageService messageService =  new MessageService();
 
-	
+	MessageService messageService = new MessageService();
+
 	@GET
-	//@Produces(MediaType.TEXT_PLAIN)
+	// @Produces(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_XML)
-	public List<Message> getMessages(){
+	public List<Message> getMessages() {
 		return messageService.getAllMessage();
+	}
+
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Message getMessage(@PathParam("messageId") long id) {
+		
+		return messageService.getMessage(id);
 	}
 
 }
